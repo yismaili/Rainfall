885125109bd779578738c22f455317a4a297975e
younes ismaili <69278312+yismaili@users.noreply.github.com>
2025-02-28 18:02:32 +0000

    Update assembly.asm

--- level1/Ressources/assembly.asm
+++ level1/Ressources/assembly.asm
@@ -1,15 +1,4 @@
-level1
-level1@RainFall:~$ gdb ./level1
-GNU gdb (Ubuntu/Linaro 7.4-2012.04-0ubuntu2.1) 7.4-2012.04
-Copyright (C) 2012 Free Software Foundation, Inc.
-License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
-This is free software: you are free to change and redistribute it.
-There is NO WARRANTY, to the extent permitted by law.  Type "show copying"
-and "show warranty" for details.
-This GDB was configured as "i686-linux-gnu".
-For bug reporting instructions, please see:
-<http://bugs.launchpad.net/gdb-linaro/>...
-Reading symbols from /home/user/level1/level1...(no debugging symbols found)...done.
+
 (gdb) disas main
 Dump of assembler code for function main:
    0x08048480 <+0>:	push   %ebp
